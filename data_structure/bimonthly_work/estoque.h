2ccbd41702ee2e0322daa6d1450597ee1aea111a
vitorkos <vitorkosejt@gmail.com>
2023-07-25 01:38:51 -0300

    chore : add lib

--- data_structure/bimonthly_work/estoque.h
+++ data_structure/bimonthly_work/estoque.h
@@ -9,9 +9,36 @@ struct produto{
 };
 
 struct lista{
-    struct produto produto;
+    struct produto *produto;
     struct lista *prox;
 };
 
 typedef struct lista Lista;
 typedef struct produto Produto;
+
+
+/* Cria um novo produto */
+Produto * CriaProduto ( char * nome , int codigo , float valor , int*data_de_validade ) ;
+
+Lista * CriaLista () ;
+
+/* Insere um produto em uma lista */
+Lista * InsereListaProduto ( Lista * l , Produto * p ) ;
+
+/* Retira um produto de uma determinada lista */
+Lista * RetiraListaProduto ( Lista * l , int id_produto ) ;
+
+/* Verifica se um produto e s t presente em uma determinada lista */
+int VerificaListaProduto ( Lista * p , int id_produto ) ;
+
+/* Verifica se existe um produto e s t vencido em uma determinadalista */
+Lista * VerificaListaValidade ( Lista * p, int d, int m, int a) ;
+
+/* Imprime todos os produtos de uma lista */
+void ImprimeListaProdutos ( Lista * p ) ;
+
+/* Orderna Lista pelo valor do produto */
+Lista * OrdenaListaValor ( Lista * p ) ;
+
+/* Orderna Lista pelo valor do produto */
+Lista * OrdenaListaVencimento ( Lista * p ) ;
