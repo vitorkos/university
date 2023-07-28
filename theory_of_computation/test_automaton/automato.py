878a89b4255a71d76d19045c14b61b34f91d2634
vitorkos <vitorkosejt@gmail.com>
2023-07-27 22:48:56 -0300

    chore : add dfa test : works 100%

--- theory_of_computation/test_automaton/automato.py
+++ theory_of_computation/test_automaton/automato.py
@@ -7,11 +7,6 @@ def readJson(json_file):
     caminho_absoluto = os.path.abspath(os.path.join(os.path.dirname(__file__), json_file))
     with open(caminho_absoluto, 'r') as file:
         automato_json = json.load(file)
-
-    # Convertendo o campo "from" para inteiros
-    for transicao in automato_json["transitions"]:
-        transicao["from"] = int(transicao["from"])
-
     return automato_json
 
 # Function to read the input file
@@ -26,38 +21,27 @@ def readInput(input_file):
             lista_dados.append((entrada, resultado_esperado))
     return lista_dados
 
-# Function to simulate the AFND using the delta transition function
-def simulateAfnd(automato, entrada):
-    # Delta function for state transitions
-    def delta(estado_atual, simbolo):
-        transicoes = [transicao for transicao in automato['transitions'] if transicao['from'] == estado_atual and transicao['read'] == simbolo]
-        estados_destino = [transicao['to'] for transicao in transicoes]
-        return estados_destino
 
-    estados_atuais = [automato['initial']]
+# Function to simulate the AFD using the delta transition function
+def simulateAfd(automato, entrada):
+    def delta(estado_atual, simbolo):
+        for transicao in automato['transitions']:
+            if int(transicao['from']) == estado_atual and transicao['read'] == simbolo:
+                return int(transicao['to'])
+        return None
+
+    estado_atual = int(automato['initial'])
 
     for simbolo in entrada:
-        novos_estados_atuais = []
-        for estado_atual in estados_atuais:
-            estados_destino = delta(estado_atual, simbolo)
-            novos_estados_atuais.extend(estados_destino)
-        
-        # Verifica se há transições vazias
-        estados_destino_vazio = delta(estado_atual, '')
-        novos_estados_atuais.extend(estados_destino_vazio)
+        estado_atual = delta(estado_atual, simbolo)
+        if estado_atual is None:
+            return False
 
-        # Atualiza os estados atuais com os novos estados
-        estados_atuais = list(set(novos_estados_atuais))
-
-    estados_finais = automato['final']
-
-    if any(estado in estados_finais for estado in estados_atuais):
-        return True
-    else:
-        return False
+    estados_finais = [int(estado) for estado in automato['final']]
+    return estado_atual in estados_finais
 
 
-
+# Main
 json_file = "automato.aut"
 automato_objeto = readJson(json_file)
 
@@ -65,5 +49,5 @@ input_file = "input.in"
 input_csv = readInput(input_file)
 
 for entrada, resultado_esperado in input_csv:
-    resultado_simulacao = simulateAfnd(automato_objeto, entrada)
-    print(f"Entrada: {entrada}, Resultado Esperado: {resultado_esperado}, Resultado Simulação: {resultado_simulacao}")
+    resultado_obtido = simulateAfd(automato_objeto, entrada)
+    print(f"Entrada: {entrada}, Resultado Esperado: {resultado_esperado}, Resultado Obtido: {resultado_obtido}")
