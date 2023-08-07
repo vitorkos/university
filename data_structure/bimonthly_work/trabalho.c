afae23313eafda72b7640e00c4e3d97928621a0f
vitorkos <vitorkosejt@gmail.com>
2023-08-07 19:04:33 -0300

    chore : add main code

--- data_structure/bimonthly_work/trabalho.c
+++ data_structure/bimonthly_work/trabalho.c
@@ -1,46 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include "estoque.h"
 
-int main(){
-    FILE *arq;
-    char linha[100];
-    char palavra[100];
+#define BUFFER 256
 
-    arq = fopen("./entrada.txt", "r");
+FILE *arquivo;
+FILE *arquivo_final;    
 
-    if(arq == NULL){
-        printf("Erro ao abrir o arquivo\n");
+//argc = quantidade de argumentos
+//argv = argumentos
+
+int main(int argc, char *argv[]){
+    
+    char leitor[20];
+
+    if(argc != 3){
+        puts("Quantidade de argumentos inválida!\n");
         return 1;
     }
-    //PERCORRE LINHA POR LINHA
-    while(fgets(linha, 100, arq) != NULL){
-        //VERIFICA A PRIMEIRA PALAVRA DA LINHA
-        if(sscanf(linha, "%s", palavra) == 1){
-            //VERIFICA QUAL A PRIMEIRA PALAVRA DA LINHA
-                if (strcmp(palavra, "PRODUTO") == 0){
-            }
-            else if (strcmp(palavra, "RETIRA") == 0){
-            }
-            else if (strcmp(palavra, "IMPRIME_LISTA") == 0){
-            }
-            else if (strcmp(palavra, "ATUALIZA_PRECO") == 0){
-            }
-            else if (strcmp(palavra, "VERIFICA_VALIDADE") == 0){
-            }
-            else if (strcmp(palavra, "VERIFICA_LISTA") == 0){
-            }
-            else if (strcmp(palavra, "ORDENA_LISTA_VALIDADE") == 0){
-            }
-            else if (strcmp(palavra, "IMPRIME_LISTA") == 0){
-            }
-            else if (strcmp(palavra, "ORDENA_LISTA_VALOR") == 0){
-            }
-            
+
+    //Abre o arquivo de entrada
+    
+    arquivo = fopen(argv[1], "r");
+
+    if(arquivo == NULL){
+        puts("Falha ao abrir o arquivo de entrada!\n");
+        perror(NULL);
+        return 1;
+    }
+    arquivo_final = fopen(argv[2], "a");
+
+    if(arquivo_final == NULL){
+        puts("Falha ao abrir o arquivo de saída!\n");
+        perror(NULL);
+        return 1;
+    }
+
+    char nome[20];
+    int codigo;
+    float valor;
+    int data[3];
+
+    //Cria a lista
+    Lista *l = CriaLista();
+
+    while(fscanf(arquivo, "%s", leitor) != EOF){
+        
+        if(!strcmp(leitor, "PRODUTO")){
+
+            fscanf(arquivo, "%s", nome);
+            fscanf(arquivo, "%d", &codigo);
+            fscanf(arquivo, "%f", &valor);
+            fscanf(arquivo, "%d", &data[0]);
+            fscanf(arquivo, "%d", &data[1]);
+            fscanf(arquivo, "%d", &data[2]);
+
+            Produto *p = CriaProduto(nome, codigo, valor, data);
+            l = InsereListaProduto(l, p);
+        }
+
+        else if(!strcmp(leitor, "RETIRA")){
+            fscanf(arquivo, "%d", &codigo);
+            l = RetiraListaProduto(l, codigo);
+        }
+
+        else if(!strcmp(leitor, "IMPRIME_LISTA")){
+            ImprimeListaProdutos(l);
+        }
+
+        else if(!strcmp(leitor, "ATUALIZA_PRECO")){
+            fscanf(arquivo, "%d", &codigo);
+            fscanf(arquivo, "%f", &valor);
+            l = AtualizaPrecoProduto(l, codigo, valor);            
+        }
+
+        else if(!strcmp(leitor, "VERIFICA_VALIDADE")){
+            fscanf(arquivo, "%d", &data[0]);
+            fscanf(arquivo, "%d", &data[1]);
+            fscanf(arquivo, "%d", &data[2]);
+            l = VerificaValidadeProdutos(l, data[0], data[1], data[2]);
+        }
+
+        else if(!strcmp(leitor, "VERIFICA_LISTA")){
+            fscanf(arquivo, "%d", &codigo);
+            int x = VerificaListaProduto(l, codigo);
+        }
+
+            else if (!strcmp(leitor, "ORDENA_LISTA_VALIDADE")) {
+        l = OrdenaListaVencimento(l);
+        }
+
+        else if (!strcmp(leitor, "ORDENA_LISTA_VALOR")) {
+            l = OrdenaListaValor(l);
+        }
+
+        else if (!strcmp(leitor, "FIM")) {
+        break;
+        }
+
+        else {
+            printf("Comando não reconhecido: %s\n", leitor);
         }
     }
-    fclose(arq);
 
+    fclose(arquivo);
+    fclose(arquivo_final);
 
     return 0;
 }
