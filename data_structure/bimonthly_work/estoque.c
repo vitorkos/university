4dd0be80870065946f0e09bd36a02defd63652f0
vitorkos <vitorkosejt@gmail.com>
2023-08-06 23:05:17 -0300

    chore :  implement functions of estoque.h lib

--- data_structure/bimonthly_work/estoque.c
+++ data_structure/bimonthly_work/estoque.c
@@ -1,106 +1,254 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include "estoque.h"
 
-//cria lista
-Lista *CriaLista(){return NULL;}
+struct produto{
+    char nome[20];
+    int codigo;
+    float valor;
+    int data[3];
 
-Produto *CriaProduto ( char * nome , int codigo , float valor , int*data_de_validade ){
-    Produto *novoProduto = (Produto *)malloc(sizeof(Produto));
-    novoProduto->nome = nome;
-    novoProduto->codigo = codigo;
-    novoProduto->valor = valor;
-    novoProduto->data = data_de_validade;
+};
 
-}
+struct lista {
+    Produto *produto;
+    Lista *prox;
+};
 
+//FUNÇÕES DE MANIPULAÇÃO DE LISTA
 
-/* Insere um produto em uma lista */
-Lista *InsereListaProduto ( Lista *l , Produto *p ){
-    Lista *novaLista = (Lista *)malloc(sizeof(Lista));
-    novaLista->produto = p;
-    novaLista->prox = l;
-}
-
-
-
-/* Retira um produto de uma determinada lista */
-Lista * RetiraListaProduto ( Lista * l , int id_produto ){
-    Lista *anterior = CriaLista();
-    Lista *novaLista = l;
-
-    while(novaLista != NULL && novaLista->produto != id_produto){
-        anterior = novaLista;
-        novaLista = novaLista->prox; 
-    }
-    if(novaLista == NULL){
-        return l;
-    }
-
-    if(anterior == NULL){
-        l = novaLista->prox;
-    } else{
-        anterior->prox = novaLista->prox;
-    }
-    free(novaLista);
-    return l;
-}
-
-
-/* Verifica se um produto e s t presente em uma determinada lista */
-int VerificaListaProduto ( Lista * p , int id_produto ){
-    Lista *lista;
-    for(lista = p; lista != NULL; lista = lista->prox){
-        if(lista->produto->codigo == id_produto){
-            return 1;
-        }
-    }
+//Cria uma lista vazia
+Lista *CriaLista(){
     return NULL;
 }
 
+//Criar um produto novo
+Produto *CriaProduto(char *nome, int codigo, float valor, int *data){
+    Produto *p = (Produto*) malloc(sizeof(Produto));
 
-/* Verifica se existe um produto e s t vencido em uma determinadalista */
-Lista * VerificaListaValidade ( Lista * p, int d, int m, int a){
-    Lista *lista;
-    
-    for(lista = p; lista != NULL; lista = lista->prox){
-        if(lista->produto->data[2] < d){
-            return p;
+    //Verifica se a alocação foi bem sucedida
+    if (p == NULL){
+        puts("Fala ao criar o produto!"\n");
+        perror(NULL);
+    }
+
+    //Copia os dados para o produto
+    p->codigo = codigo;
+    p->valor = valor;
+    for (int i = 0; i < sizeof(p->nome); i++) {
+        if (i < 3) {
+            p->data[i] = *(data_de_validade + i);
         }
-        else if(lista->produto->data[1] < m){
-            return p;
-        }
-        else if(lista->produto->data[0] < d){
-            return p;
-        }
-        else{
-            return 0;
+
+        p->nome[i] = *(nome + i);
+    }
+
+    return p;
+}
+
+//Insere um produto na lista
+Lista *InsereListaProduto(Lista *l, Produto *p){
+    Lista *novo = (Lista*) malloc(sizeof(Lista));
+
+    fseek(f_arquivo, 0, SEEK_END);
+
+    //Verifica se a alocação foi bem sucedida
+    if (novo == NULL){
+        puts("Falha ao inserir o produto!"\n");
+        perror(NULL);
+    }
+
+    //Insere o produto na lista
+    novo->produto = p;
+    novo->prox = l;
+
+    fprintf(f_arquivo, "PRODUTO %s %d ADICIONADO\n", p->nome, p->codigo);
+
+    return novo;
+}
+
+//Remove um produto da lista
+Lista *RetiraListaProduto(Lista *l, int codigo){
+    Lista *ant = CriaLista();
+    Lista *p = l;
+
+    fseek(f_arquivo, 0, SEEK_END);
+
+    //Procura o produto na lista
+    while (p != NULL && p->produto->codigo != codigo){
+        ant = p;
+        p = p->prox;
+    }
+
+    //Verifica se o produto foi encontrado
+    if (p == NULL){
+        fprintf(f_arquivo, "PRODUTO %d NAO ENCONTRADO\n", codigo);
+        return l;
+    }
+
+    //Remove o produto da lista
+    if (ant == NULL){
+        //Elemento a ser removido é o primeiro da lista
+        l = p->prox;
+    } else {
+        //Elemento a ser removido não é o primeiro da lista
+        ant->prox = p->prox;
+    }
+
+    fprintf(f_arquivo, "PRODUTO %s %d REMOVIDO\n", p->produto->nome, p->produto->codigo);
+
+    free(p->produto);
+    free(p);
+
+    return l;
+}
+
+//Verifica se um produto está na lista
+int VerificaListaProduto(Lista *l, int codigo){
+    Lista *p = l;
+
+    //Procura o produto na lista
+    while (p != NULL && p->produto->codigo != codigo){
+        p = p->prox;
+    }
+
+    //Verifica se o produto foi encontrado
+    if (p == NULL){
+        return 0;
+    }
+
+    return 1;
+}
+
+//Atualiza o preço de um produto
+Lista *AtualizaPrecoProduto(Lista *l, int codigo, float valor){
+    Lista *p = l;
+
+    //Procura o produto na lista
+    while (p != NULL && p->produto->codigo != codigo){
+        p = p->prox;
+    }
+
+    //Verifica se o produto foi encontrado
+    if (p == NULL){
+        fprintf(f_arquivo, "PRODUTO %d NAO ENCONTRADO\n", codigo);
+        return l;
+    }
+
+    //Atualiza o preço do produto
+    p->produto->valor = valor;
+
+    fprintf(f_arquivo, "PRODUTO %s %d PRECO ATUALIZADO PARA %.2f\n", p->produto->nome, p->produto->codigo, p->produto->valor);
+
+    return l;
+}
+
+//Verifica se um produto está vencido
+int VerificaValidadeProduto(Lista *l, int codigo, int dia, int mes, int ano){
+    Lista *p = l;
+
+    //Procura o produto na lista
+    while (p != NULL && p->produto->codigo != codigo){
+        p = p->prox;
+    }
+
+    //Verifica se o produto foi encontrado
+    if (p == NULL){
+        return 0;
+    }
+
+    //Verifica se o produto está vencido
+    if (p->produto->data[2] < ano){
+        return 1;
+    } else if (p->produto->data[2] == ano){
+        if (p->produto->data[1] < mes){
+            return 1;
+        } else if (p->produto->data[1] == mes){
+            if (p->produto->data[0] < dia){
+                return 1;
+            }
         }
     }
 
-
+    return 0;
 }
 
-/* Imprime todos os produtos de uma lista */
-void ImprimeListaProdutos ( Lista * p ){
+//Imprime a lista de produtos
+void ImprimeListaProdutos(Lista *l){
+    Lista *p = l;
 
-    Lista *lista ;
-    for(lista = p; lista != NULL; lista = lista->prox){
-        
+    //Verifica se a lista está vazia
+    if (p == NULL){
+        fprintf(f_arquivo, "LISTA VAZIA\n");
+        return;
+    }
 
-
-
-
+    //Imprime a lista
+    fprintf(f_arquivo, "LISTA DE PRODUTOS:\n");
+    while (p != NULL){
+        fprintf(f_arquivo, "%s %d %.2f %d/%d/%d\n", p->produto->nome, p->produto->codigo, p->produto->valor, p->produto->data[0], p->produto->data[1], p->produto->data[2]);
+        p = p->prox;
     }
 }
 
+//Ordena a lista de produtos pelo valor do produto
+Lista *OrdenaListaValor(Lista *l){
+    Lista *p;
 
+    if(l == NULL){
+        return l;
+    }
 
+    for(p = l; p->prox != NULL; p = p->prox){
+        Lista *menor = p;
+        Lista *q;
 
-/* Orderna Lista pelo valor do produto */
-Lista * OrdenaListaValor ( Lista * p ) ;
+        for(q = p->prox; q != NULL; q = q->prox){
+            if(q->produto->valor < menor->produto->valor){
+                menor = q;
+            }
+        }
 
-/* Orderna Lista pelo valor do produto */
-Lista * OrdenaListaVencimento ( Lista * p ) ;
+        if(menor != p){
+            Produto *aux = p->produto;
+            p->produto = menor->produto;
+            menor->produto = aux;
+        }
+    }
+
+    return l;
+}
+
+//Ordena a lista de produtos pela data de validade do produto
+Lista *OrdenaListaVencimento(Lista *l){
+    Lista *p;
+
+    if(l == NULL){
+        return l;
+    }
+
+    for(p = l; p->prox != NULL; p = p->prox){
+        Lista *menor = p;
+        Lista *q;
+
+        for(q = p->prox; q != NULL; q = q->prox){
+            if(q->produto->data[2] < menor->produto->data[2]){
+                menor = q;
+            } else if (q->produto->data[2] == menor->produto->data[2]){
+                if(q->produto->data[1] < menor->produto->data[1]){
+                    menor = q;
+                } else if (q->produto->data[1] == menor->produto->data[1]){
+                    if(q->produto->data[0] < menor->produto->data[0]){
+                        menor = q;
+                    }
+                }
+            }
+        }
+
+        if(menor != p){
+            Produto *aux = p->produto;
+            p->produto = menor->produto;
+            menor->produto = aux;
+        }
+    }
+
+    return l;
+}
