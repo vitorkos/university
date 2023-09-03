c0999ce8f21ca62656328b2d3ee79b2eb50fde3a
vitorkos <vitorkosejt@gmail.com>
2023-09-03 15:23:24 -0300

    chore : add list 6

---
+++ programming_2/lista6/IEmployee.java
@@ -0,0 +1,6 @@
+package com.mycompany.lista6;
+
+public interface IEmployee {
+    
+    public double calculateSalary(double h, double ph);
+}
---
+++ programming_2/lista6/Manager.java
@@ -0,0 +1,17 @@
+package com.mycompany.lista6;
+
+public class Manager implements IEmployee{
+    double hours, per_hour;
+    Manager(){
+        hours = 0.0; per_hour = 0.0;
+    }
+    Manager(double x, double y){
+        hours = x; per_hour = y;
+    }
+
+    @Override
+    public double calculateSalary(double h, double ph) {
+        return ((h * ph)*0.1) + h*ph; 
+    }
+    
+}
