06e2f196440390ed5727c720d3c1fdd242103289
vitorkos <vitorkosejt@gmail.com>
2023-09-03 23:09:49 -0300

    chore : add list 6

---
+++ programming_2/list_6/Programmer.java
@@ -0,0 +1,16 @@
+package list_6;
+
+public class Programmer implements IEmployee{
+    double hours, per_hour;
+    Programmer(){
+        hours = 0.0; per_hour = 0.0;
+    }
+    Programmer(double x, double y){
+        hours = x; per_hour = y;
+    }
+    
+    @Override
+    public double calculateSalary(double h, double ph) {
+        return h * ph;
+    }    
+}
