f97098ce01ed7821dbd0088be1efe6ba7b1b09a6
vitorkos <vitorkosejt@gmail.com>
2023-09-24 09:29:10 -0300

    chore : add second type compiled assembly

---
+++ computer_architecture/exercise_ex.s
@@ -0,0 +1,74 @@
+	.file	"exercise_ex.c"
+	.text
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Digite o valor do primeiro n\303\272mero: "
+.LC1:
+	.string	"%d"
+	.align 8
+.LC2:
+	.string	"Digite o valor do segundo n\303\272mero: "
+.LC3:
+	.string	"A soma de %d e %d \303\251: %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-20(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	leaq	.LC2(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-16(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	movl	-20(%rbp), %edx
+	movl	-16(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -12(%rbp)
+	movl	-16(%rbp), %edx
+	movl	-20(%rbp), %eax
+	movl	-12(%rbp), %ecx
+	movl	%eax, %esi
+	leaq	.LC3(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L3
+	call	__stack_chk_fail@PLT
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 13.2.1 20230801"
+	.section	.note.GNU-stack,"",@progbits
