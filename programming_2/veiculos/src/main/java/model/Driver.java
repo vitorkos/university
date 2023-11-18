49ced5be1e86160157e301be229f249761ae959c
vitorkos <vitorkosejt@gmail.com>
2023-11-18 00:33:26 -0300

    chore : implements driver class

--- programming_2/veiculos/src/main/java/model/Driver.java
+++ programming_2/veiculos/src/main/java/model/Driver.java
@@ -1,8 +1,80 @@
 package model;
 
-public class Driver {
-    private String name;
-    private String adress;
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import util.Persistent;
+
+@Entity
+@Table(name = "driver")
+@DiscriminatorValue("D")
+public class Driver extends Person implements Persistent, Serializable{
+
+    @Column
     private String cnh;
-    private String section;
+
+    @Column(length = 5)
+    private String cnhCategory;
+
+    @Column(length = 15)
+    private String sector;
+
+    public Driver() {
+    }
+
+    public Driver(String cnh, String cnhCategory, String sector, String name, String cpf, String rg, String phone, String email, String address) {
+        super(name, cpf, rg, phone, email, address);
+        this.cnh = cnh;
+        this.cnhCategory = cnhCategory;
+        this.sector = sector;
+    }
+
+    public String getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+
+    public String getCnhCategory() {
+        return cnhCategory;
+    }
+
+    public void setCnhCategory(String cnhCategory) {
+        this.cnhCategory = cnhCategory;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    @Override
+    public int hashcode(){
+        int hash = 3;
+        hash = 53 * hash + Objects.hashCode(this.cnh);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Driver other = (Driver) obj;
+        return Objects.equals(this.cnh, other.cnh);
+    }
 }
