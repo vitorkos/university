d1599454f161809517654491efe1f239d3cbe0c3
vitorkos <vitorkosejt@gmail.com>
2023-11-18 00:50:07 -0300

    chore : implements operator class

--- programming_2/veiculos/src/main/java/model/Person.java
+++ programming_2/veiculos/src/main/java/model/Person.java
@@ -1,5 +1,14 @@
 package model;
 
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import util.Persistent;
+
+@Entity
 public class Person {
     
 }
