66da447f7bf0a6398634b0c6d3455145b5a227b3
vitorkos <vitorkosejt@gmail.com>
2023-11-20 17:21:30 -0300

    chore : add bubble sort implementation

---
+++ data_structure/implementations/ordenate-algorithms/BubbleSort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define troca(A, B){struct item c = A; A = B ; B = c;}
+
+struct item{
+    int valor;
+};
+
+typedef struct item Item;
+
+void bubbleSort(Item* v, int n){
+    int i, j;
+    for(i = 0; i < n-1; i++){
+        for(j = 1; j < n-i; j++){
+            if(v[j].valor < v[j-1].valor){
+                troca(v[j-1], v[j]);
+            }
+        }
+    }
+}
+
+int main(){
+    int n; scanf("%d", &n);
+    Item* itens = (Item *)malloc(n * sizeof(Item));
+    for(int i = 0; i < n; i++){
+        scanf("%d", &itens[i].valor);
+    }
+    bubbleSort(itens, n);
+    for(int i = 0; i < n; i++){
+        printf("%d\n", itens[i].valor);
+    }
+    free(itens); return 0;
+}
