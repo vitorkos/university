bd2de288ef5ab94833c5dbfcbe6b2996a2a7dc5e
vitorkos <vitorkosejt@gmail.com>
2023-11-27 21:37:15 -0300

    feat : main test instance

--- programming_2/list9_generic/Drawer.java
+++ programming_2/list9_generic/Drawer.java
@@ -19,5 +19,10 @@ public class Drawer<T> {
 
     public static void main(String[] args) {
         Drawer<String> drawer = new Drawer<>("Oi");
+        drawer.getContent();
+        Drawer<Integer> drawer2 = new Drawer<>(2);
+        drawer2.setContent(213);
+        drawer2.getContent();
+        System.out.println(drawer2.getContent());
     }
 }
