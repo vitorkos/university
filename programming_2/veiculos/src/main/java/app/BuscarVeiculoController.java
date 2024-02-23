5b2c78a033032cd1446479a007cb665a077fc1cc
vitorkos <vitorkosejt@gmail.com>
2024-02-23 16:04:41 -0300

    feat : buscarveiculomotorista method(search for the driver who used the vehicle in the date)

---
+++ programming_2/veiculos/src/main/java/app/BuscarVeiculoController.java
@@ -0,0 +1,77 @@
+package app;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import model.Motorista;
+import model.Veiculo;
+import model.UsoVeiculo;
+import util.Dao;
+import java.io.Console;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class BuscarVeiculoController {
+    @FXML
+    private ListView<String> drivers;
+
+    @FXML
+    private ComboBox<Veiculo> vehicles;
+
+    @FXML
+    private DatePicker datePicker;
+
+    private Dao<Veiculo> daoVehicle;
+
+    private Dao<UsoVeiculo> daoVehicleUse;
+
+    @FXML
+    private void initialize() {
+        daoVehicle = new Dao(Veiculo.class);
+        daoVehicleUse = new Dao(UsoVeiculo.class);
+
+        List<Veiculo> getVehicles = daoVehicle.getAll();
+        ObservableList<Veiculo> getVehiclesObservable = FXCollections.observableArrayList(getVehicles);
+        vehicles.setItems(getVehiclesObservable);
+    }
+
+    @FXML
+    private void BuscarVeiculoMotorista() {
+        Veiculo vehicle = vehicles.getSelectionModel().getSelectedItem();
+        LocalDate date = datePicker.getValue();
+
+        List<String> drivers = FXCollections.observableArrayList();
+
+        try {
+            List<UsoVeiculo> vehiclesUseList = daoVehicleUse.getAll();
+
+            for (UsoVeiculo vehicleUse : vehiclesUseList) {
+                if (String.valueOf(vehicle).equals(String.valueOf(vehicleUse.getVehicle()))) {
+                    if (String.valueOf(vehicleUse.getWithdraw()).equals(String.valueOf(date))) {
+                        drivers.add(vehicleUse.getDriver().getName() + " " + vehicleUse.getDriver().getCnh());
+                    }
+                }
+            }
+
+            this.drivers.getItems().addAll(drivers);
+
+            if (drivers.isEmpty()) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText("Nenhum motorista retirou o veículo");
+                alert.show();
+            }
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Erro na busca pelo veículo");
+            alert.show();
+        }
+    }
+
+}
