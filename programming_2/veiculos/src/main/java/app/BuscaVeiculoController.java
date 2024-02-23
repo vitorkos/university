9b04630af2dbd8bc01ec524b609e80b1569db4bd
vitorkos <vitorkosejt@gmail.com>
2024-02-23 15:51:59 -0300

    chore : busca veiculo controller

---
+++ programming_2/veiculos/src/main/java/app/BuscaVeiculoController.java
@@ -0,0 +1,23 @@
+package app;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import model.Motorista;
+import model.Veiculo;
+import model.UsoVeiculo;
+import util.Dao;
+import java.io.Console;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class BuscaVeiculoController {
+    
+}
