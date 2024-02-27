c436cf9b273756cd44e63a07a6dafc68de4fac17
vitorkos <vitorkosejt@gmail.com>
2024-02-27 11:06:19 -0300

    feat : hashing method

--- data_structure/hashmap-implementation/HashMap.c
+++ data_structure/hashmap-implementation/HashMap.c
@@ -10,15 +10,31 @@
 
 //transformação de chaves não numericas 
 //cria um peso para cada caractere
-void geraPesos(/*vetor dos pesos*/int p[],/*tamanho da chave*/ int n){
+
+    /*vetor dos pesos*//*tamanho da chave*/ 
+void geraPesos(int p[],int n){
     //utiliza o tempo como gerador dos valores aleatorios
     srand(time(NULL));
     for(int i = 0; i < n; i++){
         // itera e cria um peso para cada caractere
         p[i] = 1 + (int) (10000.0 * rand() / RAND_MAX);
     }
-};
+}
 
+//função de hashing ( retorna o indice daquela chave )
+    /*chave*/ /*vetor de pesos*/ /*tamanho do vetor*/ /*tamanho do vetor pesos*/ 
+int h(char *chave, int p[], int m, int tam_p){
+    unsigned int soma = 0;
+    /*comprimento da chave*/
+    int comp = strlen(chave);
+    for(int i = 0; i < comp; i++){
+        /*itera sob o comprimento da chave*/
+        /*a cada iteração, incrementa a soma com (caractere i * resto de peso da posicao i / tamanho do vetor p)*/
+        soma += (unsigned int) chave [i] * p[i % tam_p];
+    }
+    //retorna o resto da soma pelo tamanho do vetor de endereços
+    return soma % m;
+}
 
 
 
